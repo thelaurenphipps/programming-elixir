bfc0ce9f6cd25e895ee7a855d9cd234bda13c6d0
Lauren Phipps <lkoperski@unomaha.edu>
2022-06-01 10:45:10 -0500

    rename chapter oflder

--- chapter4/functions4.exs
+++
@@ -1,8 +0,0 @@
-prefix = fn string1 -> fn string2 -> "#{string1} #{string2}" end end
-
-mrs = prefix.("Mrs")
-
-IO.puts mrs.("Smith")
-
-IO.puts prefix.("Elixir").("Rocks")
-
--- chapter4/functions5.exs
+++
@@ -1,3 +0,0 @@
-add_two = fn list -> &(&1 +2) end
-
-IO.puts add_two.([1, 2, 3, 4])
