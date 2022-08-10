0e007cfbd4c000f1c224b5ba31828f360784e8e2
Lauren Phipps <lkoperski@unomaha.edu>
2022-08-10 12:03:21 -0500

    chapter 5 exercises

---
+++ chapter5/functions_1.exs
@@ -0,0 +1,13 @@
+# Create and run the functions to do the following:
+# list_concat.([:a, :b], [:c, :d]) # => [:a, :b, :c, :d]
+# sum.(1, 2, 3) # => 6
+# pair_tuple_to_list.({1234, 5678}) # => [ 1234, 5678 ]
+
+list_concat = fn a, b -> a ++ b end
+IO.inspect list_concat.([:a, :b], [:c, :d])
+
+sum = fn a, b, c -> a + b + c end
+IO.inspect sum.(1, 2, 3)
+
+pair_tuple_to_list = fn {a, b} -> [a, b] end
+IO.inspect pair_tuple_to_list.({1234, 5678})
--- chapter5/functions_5.exs
+++ chapter5/functions_5.exs
@@ -2,6 +2,5 @@
 # - Enum.map [1, 2, 3, 4], fn x -> x + 2 end
 # - Enum.each [1, 2, 3, 4], fn x -> IO.inspect x end
 
-add_two = fn list -> &(&1 +2) end
-
-IO.puts add_two.([1, 2, 3, 4])
+IO.inspect Enum.map [1, 2, 3, 4], &(&1 + 2)
+IO.inspect Enum.map [1, 2, 3, 4], &(IO.inspect &1)
