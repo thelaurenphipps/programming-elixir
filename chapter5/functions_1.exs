a1e79559360346ffaaca87c18a6513f381eae70a
Lauren Phipps <lkoperski@unomaha.edu>
2022-08-10 14:17:06 -0500

    outline exercise

--- chapter5/functions_1.exs
+++ chapter5/functions_1.exs
@@ -1,7 +1,7 @@
 # Create and run the functions to do the following:
-# list_concat.([:a, :b], [:c, :d]) # => [:a, :b, :c, :d]
-# sum.(1, 2, 3) # => 6
-# pair_tuple_to_list.({1234, 5678}) # => [ 1234, 5678 ]
+# - list_concat.([:a, :b], [:c, :d]) # => [:a, :b, :c, :d]
+# - sum.(1, 2, 3) # => 6
+# - pair_tuple_to_list.({1234, 5678}) # => [ 1234, 5678 ]
 
 list_concat = fn a, b -> a ++ b end
 IO.inspect list_concat.([:a, :b], [:c, :d])
---
+++ chapter6/modules_and_functions_7.exs
@@ -0,0 +1,7 @@
+# Find the library functions to do the following and then use each in IEx
+# - Convert a float to a string with two decimal digits (Erlang)
+# - Get the value of an operating system environment variable (Elixir)
+# - Return the extension component of a file name (so return .exs if given "dave/test.exs") (Elicir)
+# - Return the process's current working directory (Elixir)
+# - Convert a string containing JSON into Elixir data strucutres (Just find; don't install)
+# - Execute a comman in your operating system's shell
